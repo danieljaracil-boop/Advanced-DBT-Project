6050665d7b4b7712471c76e21a2802f2c1506ee5
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-02-24 17:24:14 +0100

    Add staging models for customers and orders with appropriate configurations

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    c_custkey AS customer_id,
+    c_name AS customer_name,
+    c_address AS address,
+    c_nationkey AS nation_id,
+    c_phone AS phone_number
+FROM {{ source('tpch_source', 'customer') }}
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='o_orderkey'
+    )
+}}
+
+SELECT
+    o_orderkey AS order_id,
+    o_custkey AS customer_id,
+    o_orderstatus AS status_code,
+    o_totalprice AS total_price,
+    o_orderdate AS order_date
+FROM {{ source('tpch_source', 'orders') }}
+
+{% if is_incremental() %}
+  WHERE o_orderdate >= (select max(order_date) from {{ this }})
+{% endif %}
