d5ba6d125053fe07bf4f1c8ff5ba16ef9d797554
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-02-27 13:32:40 +0100

    Add order_priority case statement to stg_orders.sql for enhanced order classification

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -9,6 +9,14 @@ SELECT
     o_orderkey AS order_id,
     o_custkey AS customer_id,
     o_orderstatus AS status_code,
+    case 
+            when o_orderpriority like '1%' then 'Urgent'
+            when o_orderpriority like '2%' then 'High'
+            when o_orderpriority like '3%' then 'Medium'
+            when o_orderpriority like '4%' then 'Not Specified'
+            when o_orderpriority like '5%' then 'Low'
+            else 'NS'
+        end as order_priority,
     o_totalprice AS total_price,
     o_orderdate AS order_date
 FROM {{ source('tpch_source', 'orders') }}
