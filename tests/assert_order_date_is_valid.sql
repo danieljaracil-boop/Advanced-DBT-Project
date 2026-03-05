e2a29785215e759380fadc5221a206a28fde815b
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-03-05 15:25:23 +0100

    adding index.html file to try and fix dbt docs serve malfuncitoning

--- tests/assert_order_date_is_valid.sql
+++ tests/assert_order_date_is_valid.sql
@@ -1,8 +1,8 @@
 -- Regla de negocio: La fecha del pedido no puede ser futura
 -- y debe ser posterior a la fundación de la empresa (ej. 1990)
 select
-    o_orderkey,
-    o_orderdate
+    order_id,
+    order_date
 from {{ ref('stg_orders') }}
-where o_orderdate > current_date
-   or o_orderdate < '1990-01-01'
+where order_date > current_date
+   or order_date < '1990-01-01'
--- tests/assert_total_sales_amount_is_positive.sql
+++ tests/assert_total_sales_amount_is_positive.sql
@@ -2,6 +2,6 @@
 -- en el modelo final nunca debe ser negativo.
 select
     customer_key,
-    total_spent
-from {{ ref('fct_orders') }}
+    total_order_amount as total_spent
+from {{ ref('fct_sales') }}
 where total_spent < 0
