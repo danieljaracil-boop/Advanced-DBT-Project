7eec3ecb48815f222a9814f1aac735735a833025
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-03-05 12:58:15 +0100

    --

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
 
-    {# Si hay un custom_schema (como staging o marts), concaténalo #}
     {%- if custom_schema_name is not none -%}
         {{ default_schema }}_{{ custom_schema_name | trim }}
     {%- else -%}
