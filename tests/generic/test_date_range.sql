f558948793b04490f9a183a04fbb7270676b0ad0
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-03-10 12:47:44 +0100

    feat: implementación de test generico de comprobación de fechas

--- tests/generic/test_date_range.sql
+++ tests/generic/test_date_range.sql
@@ -1,6 +1,6 @@
--- tests/assert_order_date_is_recent.sql
 select
     order_id,
     order_date
 from {{ ref('stg_orders') }}
-where order_date > current_date() or order_date < '2020-01-01'
+where order_date > current_date()
+   or order_date < '1990-01-01'
