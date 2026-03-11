f2cf0a4c58dbd10c59e15450f8b7891c8e50e4e4
danieljaracil-boop <danieljaracil@sdggroup.com>
2026-03-11 09:54:11 +0100

    docs: fixing fct_sales referencing in test_consistencia_ventas

--- tests/business/test_consistencia_ventas.sql
+++ tests/business/test_consistencia_ventas.sql
@@ -1,5 +1,5 @@
 select
     order_id,
     total_amount
-from {{ ref('fct_orders') }} -- Ajusta al nombre de tu modelo de ventas
+from {{ ref('fct_sales') }} -- Ajusta al nombre de tu modelo de ventas
 where total_amount < 0
